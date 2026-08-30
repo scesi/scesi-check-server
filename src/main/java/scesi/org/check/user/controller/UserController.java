d0a922fbe74452df69b7ef2fa5a78a7aff105ffe
zer4c <pabloazurduy40@gmail.com>
2026-08-29 21:30:40 -0400

    feat: updated UserController file and created StandardResponse

--- src/main/java/scesi/org/check/user/controller/UserController.java
+++ src/main/java/scesi/org/check/user/controller/UserController.java
@@ -1,42 +1,111 @@
 package scesi.org.check.user.controller;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+
 import org.springframework.http.HttpStatus;
-
-import jakarta.validation.Valid;
-import scesi.org.check.user.service.UserServiceImpl;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import scesi.org.check.core.model.response.StandardResponse;
 import scesi.org.check.user.model.entity.User;
+import scesi.org.check.user.model.request.CreateUserRequest;
+import scesi.org.check.user.model.request.UpdateUserRequest;
+import scesi.org.check.user.model.response.UserResponse;
+import scesi.org.check.user.service.IUserService;
 
-import java.util.Optional;
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    @Autowired
-    private UserServiceImpl userService;
 
-    @PostMapping
-    public ResponseEntity<User> saveUser(@RequestBody User user) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(userService.saveUser(user));
+    private IUserService iUserService;
+
+    public UserController(IUserService iUserService) {
+        this.iUserService = iUserService;
     }
 
-    @DeleteMapping(value ="/{id}")
-    public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
-        if (!userService.deleteUser(id)) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.noContent().build();
+    @GetMapping("/{userId}")
+    public ResponseEntity<StandardResponse<UserResponse>> getUserById(
+            // @Validated
+            // probar si se necesita la validacion de positivo o nulo
+            @PathVariable("userId") final long userId
+    ) {
+        final User user = iUserService.getUserById(userId);
+        final UserResponse userResponse = generateUserResponse(user);
+        final StandardResponse<UserResponse> standardResponse = StandardResponse.<UserResponse>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("user retrieved successfully")
+                .data(userResponse)
+                .build();
+        return ResponseEntity.status(HttpStatus.OK).body(standardResponse);
     }
 
-    @GetMapping(value = "/{id}")
-    public ResponseEntity<User> getUserById(@PathVariable Long id) {
-        return ResponseEntity.status(HttpStatus.OK).body(userService.getUserById(id));
+    @GetMapping("/")
+    public ResponseEntity<StandardResponse<List<UserResponse>>> getAllUsers() {
+        final List<User> user = iUserService.getAllUsers();
+        final List<UserResponse> userListResponse = user.stream()
+                .map(this::generateUserResponse)
+                .toList();
+        final StandardResponse<List<UserResponse>> standardResponse = StandardResponse.<List<UserResponse>>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("Users Retrieved")
+                .data(userListResponse)
+                .build();
+        return ResponseEntity.status(HttpStatus.OK).body(standardResponse);
     }
 
-    @PutMapping
-    public ResponseEntity<User> editUser(@Valid @RequestBody User user) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(userService.editUser(user));
+    @PostMapping("/")
+    public ResponseEntity<StandardResponse<UserResponse>> createUser(
+            @Validated
+            @RequestBody final CreateUserRequest request
+    ) {
+        final User userCreated = iUserService.createUser(request);
+        final UserResponse userResponse = generateUserResponse(userCreated);
+        final StandardResponse<UserResponse> standardResponse = StandardResponse.<UserResponse>builder()
+                .statusCode(HttpStatus.CREATED.value())
+                .message("User created successfully")
+                .data(userResponse)
+                .build();
+        return ResponseEntity.status(HttpStatus.CREATED).body(standardResponse);
+    }
+
+    @DeleteMapping("/{userId}")
+    public ResponseEntity<StandardResponse<Boolean>> deleteUser(
+            @PathVariable("userId") final Long userId
+    ) {
+        final Boolean userDeletedResult = iUserService.deleteUser(userId);
+        final StandardResponse<Boolean> standardResponse = StandardResponse.<Boolean>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("User deleted successfully")
+                .data(userDeletedResult)
+                .build();
+        return ResponseEntity.status(HttpStatus.OK).body(standardResponse);
+    }
+    
+    @PatchMapping("/{userId}")
+    public ResponseEntity<StandardResponse<UserResponse>> updateUser(
+            @PathVariable("userId") final Long userId,
+            @Validated
+            @RequestBody final UpdateUserRequest request
+    ){
+        final User user = iUserService.updateUser(userId, request);
+        final UserResponse userResponse = generateUserResponse(user);
+        final StandardResponse<UserResponse> standardResponse = StandardResponse.<UserResponse>builder()
+                .statusCode(HttpStatus.OK.value())
+                .message("User updated successfully")
+                .data(userResponse)
+                .build();
+        return ResponseEntity.status(HttpStatus.OK).body(standardResponse);
+    }
+
+    private UserResponse generateUserResponse(User user) {
+        return UserResponse.builder()
+                .id(user.getId())
+                .name(user.getName())
+                .lastName(user.getLastName())
+                .email(user.getEmail())
+                .active(user.getActive())
+                .build();
     }
 
 }
