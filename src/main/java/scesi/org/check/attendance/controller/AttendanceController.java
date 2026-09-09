4d2d6e737442ca11a63cd224c91b9e5c3ae9ba81
zer4c <pabloazurduy40@gmail.com>
2026-09-09 00:02:32 -0400

    refactor: removed unused imports

--- src/main/java/scesi/org/check/attendance/controller/AttendanceController.java
+++ src/main/java/scesi/org/check/attendance/controller/AttendanceController.java
@@ -1,6 +1,5 @@
 package scesi.org.check.attendance.controller;
 
-import org.apache.coyote.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
