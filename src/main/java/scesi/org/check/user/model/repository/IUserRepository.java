587694fe33569c00cb734b724964f4ee15ea36c6
zer4c <pabloazurduy40@gmail.com>
2026-09-09 20:23:29 -0400

    refactor: removed unused imports

--- src/main/java/scesi/org/check/user/model/repository/IUserRepository.java
+++ src/main/java/scesi/org/check/user/model/repository/IUserRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import scesi.org.check.user.model.entity.User;
 
-import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
