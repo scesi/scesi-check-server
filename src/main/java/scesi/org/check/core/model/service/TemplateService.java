0b8d8127cf0b99f6e1598d0361b29ea6b5782b09
zer4c <pabloazurduy40@gmail.com>
2026-09-13 14:48:00 -0400

    feat: create TemplateService to get templates.

---
+++ src/main/java/scesi/org/check/core/model/service/TemplateService.java
@@ -0,0 +1,25 @@
+package scesi.org.check.core.model.service;
+
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.util.Map;
+
+@Service
+public class TemplateService {
+
+    private final TemplateEngine templateEngine;
+
+    public TemplateService(TemplateEngine templateEngine) {
+        this.templateEngine = templateEngine;
+    }
+
+    public String getTemplate(String templatePath, Map<String, Object> variables) {
+        Context context = new Context();
+        if (variables != null) {
+            context.setVariables(variables);
+        }
+        return templateEngine.process(templatePath, context);
+    }
+}
